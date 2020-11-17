d14c5d5dcc823bc10e5a7feca1c8ba9a5561005e
BehzadShahid319 <behzadshahid95@gmail.com>
2020-11-17 17:01:24 +0500

    ReactJS GetPostAPIs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+          error: null,
+          isLoaded: false,
+          items: [],
+          flag:0
+        };
+  }
+
+  GET=()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      console.log(response)
+      this.setState({isLoaded:true,
+                      items:response.data,
+                      flag:1
+                    })
+    })
+    .catch(error => {
+      alert("error");
+      console.log(error);
+    })
+  }
+
+POST=()=>{
+  console.log('Console Testing');
+  var dict = { userId: document.getElementById('name').value,
+               body:document.getElementById('age').value, 
+               title: document.getElementById('email').value };
+  axios.post('https://jsonplaceholder.typicode.com/posts',dict)
+  .then(response => {
+    console.log(response)
+    this.setState({isLoaded:true,
+                    flag:2
+                  })
+  })
+  .catch(error => {
+    alert(error);
+    console.log(error);
+  })
+
+}
+check=()=>{
+  this.setState({isLoaded:true,
+    flag:15
+  })
+}
+
+  render() {
+
+    const { error, isLoaded, items } = this.state;
+
+    if (this.state.flag==1){
+
+    return(
+      <div>
+
+      <button onClick={this.GET}>
+      GET API CALL
+      </button>
+
+      <button onClick={this.check}>
+      POST API CALL
+      </button>
+
+      <h1> GET API RESULTS AFTER BUTTON CLICK </h1>
+
+      <ul>
+           {items.map(item => (
+             <li key={item.name}>
+               {item.id} {item.title}
+             </li>
+           ))}
+      </ul>
+
+      </div>
+
+      );
+
+
+    }else if (this.state.flag==2){
+
+      return(
+      <div>
+      <button onClick={this.GET}>
+      GET API CALL
+      </button>
+
+      <button onClick={this.check}>
+      POST API CALL
+      </button>
+      <h1> Post Request Submitted Successfully</h1>
+
+      </div>
+
+      );
+
+
+    }
+    else if (this.state.flag==15)
+    {
+      return(
+        <div>
+          <input id='name' type='text'></input>Name <br></br>
+          <input id='age' type='text'></input>Age<br></br>
+          <input id='email' type='text'></input>Email<br></br>
+          <button onClick={this.POST}>
+            Submit
+          </button>
+        </div>
+      )
+    }
+    else{
+
+      return(
+      <div>
+
+      <button onClick={this.GET}>
+      GET API CALL
+      </button>
+
+      <button onClick={this.check}>
+      POST API CALL
+      </button>
+
+      </div>
+
+      );
+
+    }
+
+  }
+
+  componentDidMount() {
+
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      console.log(response)
+      this.setState({isLoaded:true,
+                      items:response.data})
+    })
+    .catch(error => {
+      alert("error");
+      console.log(error);
+    })
+    }
 }
 
 export default App;
